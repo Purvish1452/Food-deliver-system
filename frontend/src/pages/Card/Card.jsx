2601d582d97bb2587e577414374cd300ded61dc7
Panchal Purvish Pankajkumar <152481999+Purvish1452@users.noreply.github.com>
2025-08-03 10:34:16 +0530

    Update Card.jsx

--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
+import { food_list } from "../../assets/assets";
 const Card = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
-    <div>
+  <div className="cart">
+    <div className="cart-items">
+      <div className="cart-items-title">
+           <p>Item</p>
+           <p>Title</p>
+           <p>Price</p>
+           <p>Quantity</p>
+           <p>Total</p>
+           <p>Remove</p>
+      </div>
+      <br/>
+      <hr/>
       
     </div>
-  )
-}
 
-export default Card
+
+  </div>
+  )
+};
+
+export default Card;
