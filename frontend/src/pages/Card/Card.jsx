66a3cfa82c4badc61d06b734178e009e4980ec93
Panchal Purvish Pankajkumar <152481999+Purvish1452@users.noreply.github.com>
2025-08-03 10:41:58 +0530

    cart page

--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 import "./Card.css";
-import { food_list } from "../../assets/assets";
+
 const Card = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cardItems, food_list, removeFromCard } = useContext(StoreContext);
+
   return (
-  <div className="cart">
-    <div className="cart-items">
-      <div className="cart-items-title">
-           <p>Item</p>
-           <p>Title</p>
-           <p>Price</p>
-           <p>Quantity</p>
-           <p>Total</p>
-           <p>Remove</p>
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Item</p>
+          <p>Name</p>
+          <p>Price</p>
+        </div>
+
+        {food_list.map((item) => {
+          if (cardItems[item.id] > 0) {
+            return (
+              <div key={item.id} className="cart-item">
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>₹{item.price}</p>
+                <p>Qty: {cardItems[item.id]}</p>
+                <button onClick={() => removeFromCard(item.id)}>Remove</button>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
-      <br/>
-      <hr/>
-      
     </div>
-
-
-  </div>
-  )
+  );
 };
 
 export default Card;
