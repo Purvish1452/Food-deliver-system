c47b8d1fe77baba368ee07d951e1b79524277491
Panchal Purvish Pankajkumar <152481999+Purvish1452@users.noreply.github.com>
2025-08-31 15:39:45 +0530

    ADD ITEMS ,LIST ITEMS,ORDER ITMES

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/sidebar/sidebar";
+import { Route, Router, Routes } from "react-router-dom";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import Add from "./pages/Add/Add";
 const App = () => {
   return (
     <div>
@@ -8,6 +12,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/List" element={<List />} />
+          <Route path="/Orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
   );
